83e8ae0cf2e5d7ee60321c37a8138dfcdff83033
Simon Lee <lempel@gmail.com>
2013-07-10 10:56:48 +0900

    .

---
+++ tests-to-do/src/todo/tests/LastModifiedTest.java
@@ -0,0 +1,22 @@
+package todo.tests;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class LastModifiedTest {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		File f = new File("test.txt");
+		FileOutputStream fos = new FileOutputStream(f);
+		fos.close();
+
+		System.out.println(f.lastModified());
+
+		Thread.sleep(2000);
+
+		fos = new FileOutputStream(f);
+		fos.close();
+
+		System.out.println(f.lastModified());
+	}
+}
--- tests-to-do/src/todo/tests/ListTest.java
+++ tests-to-do/src/todo/tests/ListTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package todo.tests;
 
 import java.util.Iterator;
