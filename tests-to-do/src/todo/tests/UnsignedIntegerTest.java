58c628a420f688881b043d83c30c862dedd187fb
Sangmin Lee <lempel@gmail.com>
2015-12-16 14:34:05 +0900

    new test for UnsignedInteger

---
+++ tests-to-do/src/todo/tests/UnsignedIntegerTest.java
@@ -0,0 +1,48 @@
+package todo.tests;
+
+import blueprint.sdk.util.lang.UnsignedInteger;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class UnsignedIntegerTest {
+    public static void main(String[] args) throws IOException {
+        byte[] little = new byte[]{0x01, 0x00, 0x00, 0x00};
+        byte[] big = new byte[]{0x00, 0x00, 0x00, 0x01};
+
+        DataInputStream littleStream = new DataInputStream(new ByteArrayInputStream(little));
+        DataInputStream bigStream = new DataInputStream(new ByteArrayInputStream(big));
+        System.out.println("readInt(little) = " + littleStream.readInt());
+        System.out.println("readInt(big) = " + bigStream.readInt());
+
+        System.out.println("intValue(little) = " + UnsignedInteger.intValue(little, true));
+        System.out.println("intValue(big) = " + UnsignedInteger.intValue(big, false));
+        System.out.println();
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        little = new byte[]{0x00, 0x00, 0x00, (byte) 0xFF};
+        big = new byte[]{(byte) 0xFF, 0x00, 0x00, 0x00};
+
+        littleStream = new DataInputStream(new ByteArrayInputStream(little));
+        bigStream = new DataInputStream(new ByteArrayInputStream(big));
+        System.out.println("readInt(unsigned little) = " + littleStream.readInt());
+        System.out.println("readInt(unsigned big) = " + bigStream.readInt());
+
+        try {
+            System.out.println("intValue(unsigned little) = " + UnsignedInteger.intValue(little, true));
+        } catch (NumberFormatException e) {
+            System.out.println("intValue(unsigned little) = can't be expressed as singed int");
+        }
+        try {
+            System.out.println("intValue(unsigned big) = " + UnsignedInteger.intValue(big, false));
+        } catch (NumberFormatException e) {
+            System.out.println("intValue(unsigned little) = can't be expressed as singed int");
+        }
+        System.out.println("longValue(unsigned little) = " + UnsignedInteger.longValue(little, true));
+        System.out.println("longValue(unsigned big) = " + UnsignedInteger.longValue(big, false));
+
+        System.out.println("longValue(unsigned little) = 0x" + Long.toHexString(UnsignedInteger.longValue(little, true)));
+    }
+}
